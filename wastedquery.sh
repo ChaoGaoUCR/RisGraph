d01eda47ec8efb9f6d068c6643d9d022f61813d5
ChaoGaoUCR <cgao037@ucr.edu>
2025-03-21 21:01:01 +0000

    new file:   figue1b.txt 	modified:   src/accuracy-degration.cpp 	new file:   wastedquery.sh

---
+++ wastedquery.sh
@@ -0,0 +1,6 @@
+./build/wastedquery ~/graph/ttw/ttw_snap source/TW_queries.txt 1 0.01 
+./build/wastedquery ~/graph/ttw/ttw_snap source/TW_queries.txt 1 0.02
+./build/wastedquery ~/graph/ttw/ttw_snap source/TW_queries.txt 1 0.04
+./build/wastedquery ~/graph/ttw/ttw_snap source/TW_queries.txt 1 0.08
+./build/wastedquery ~/graph/ttw/ttw_snap source/TW_queries.txt 1 0.16
+./build/wastedquery ~/graph/ttw/ttw_snap source/TW_queries.txt 1 0.32
