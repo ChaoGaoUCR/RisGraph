e6b4c79b70e98e4783cbdde3d4324b02ce15496b
ChaoGaoUCR <cgao037@ucr.edu>
2025-03-21 19:35:12 +0000

    new file:   figure1a.txt 	modified:   march16all.txt 	modified:   motivation.sh 	new file:   new_motivation_ttw.txt 	modified:   src/motivation_data.cpp 	modified:   src/stream_common_sssp.cpp

--- motivation.sh
+++ motivation.sh
@@ -1,3 +1,16 @@
+./build/motivation ~/graph/wiki/snap_wiki source/wiki_src.txt 16 0.02 4
+./build/motivation ~/graph/wiki/snap_wiki source/wiki_src.txt 16 0.02 8
+./build/motivation ~/graph/wiki/snap_wiki source/wiki_src.txt 16 0.02 12
+./build/motivation ~/graph/wiki/snap_wiki source/wiki_src.txt 16 0.02 16
+./build/motivation ~/graph/wiki/snap_wiki source/wiki_src.txt 16 0.02 20
+./build/motivation ~/graph/wiki/snap_wiki source/wiki_src.txt 16 0.02 24
+./build/motivation ~/graph/wiki/snap_wiki source/wiki_src.txt 16 0.02 32
+
+
 ./build/motivation ~/graph/ttw/ttw_snap source/TW_queries.txt 16 0.02 4
 ./build/motivation ~/graph/ttw/ttw_snap source/TW_queries.txt 16 0.02 8
+./build/motivation ~/graph/ttw/ttw_snap source/TW_queries.txt 16 0.02 12
 ./build/motivation ~/graph/ttw/ttw_snap source/TW_queries.txt 16 0.02 16
+./build/motivation ~/graph/ttw/ttw_snap source/TW_queries.txt 16 0.02 20
+./build/motivation ~/graph/ttw/ttw_snap source/TW_queries.txt 16 0.02 24
+./build/motivation ~/graph/ttw/ttw_snap source/TW_queries.txt 16 0.02 32
