e21eb933d8a0542ce20ec0c458dea957bb74f10d
ChaoGaoUCR <cgao037@ucr.edu>
2025-03-22 01:53:29 +0000

    new file:   tw_core.txt 	new file:   wastedmore.txt 	modified:   wastedquery.sh

--- wastedquery.sh
+++ wastedquery.sh
@@ -1,6 +1,26 @@
-./build/wastedquery ~/graph/ttw/ttw_snap source/TW_queries.txt 1 0.01 
-./build/wastedquery ~/graph/ttw/ttw_snap source/TW_queries.txt 1 0.02
-./build/wastedquery ~/graph/ttw/ttw_snap source/TW_queries.txt 1 0.04
-./build/wastedquery ~/graph/ttw/ttw_snap source/TW_queries.txt 1 0.08
-./build/wastedquery ~/graph/ttw/ttw_snap source/TW_queries.txt 1 0.16
-./build/wastedquery ~/graph/ttw/ttw_snap source/TW_queries.txt 1 0.32
+./build/wastedquery /home/cgao037/graph/sx/stack_snap source/sx.txt 1 0.01
+./build/wastedquery /home/cgao037/graph/sx/stack_snap source/sx.txt 1 0.02
+./build/wastedquery /home/cgao037/graph/sx/stack_snap source/sx.txt 1 0.04
+./build/wastedquery /home/cgao037/graph/sx/stack_snap source/sx.txt 1 0.08
+./build/wastedquery /home/cgao037/graph/sx/stack_snap source/sx.txt 1 0.16
+
+./build/wastedquery /home/cgao037/graph/or/snap_or source/or.txt 1 0.01 
+./build/wastedquery /home/cgao037/graph/or/snap_or source/or.txt 1 0.02
+./build/wastedquery /home/cgao037/graph/or/snap_or source/or.txt 1 0.04
+./build/wastedquery /home/cgao037/graph/or/snap_or source/or.txt 1 0.08
+./build/wastedquery /home/cgao037/graph/or/snap_or source/or.txt 1 0.16
+
+./build/wastedquery /home/cgao037/graph/dl/snap_dli source/or.txt 1 0.01
+./build/wastedquery /home/cgao037/graph/dl/snap_dli source/or.txt 1 0.02 
+./build/wastedquery /home/cgao037/graph/dl/snap_dli source/or.txt 1 0.04 
+./build/wastedquery /home/cgao037/graph/dl/snap_dli source/or.txt 1 0.08 
+./build/wastedquery /home/cgao037/graph/dl/snap_dli source/or.txt 1 0.16  
+
+
+
+
+
+
+
+
+
