f568482659faa4b8dfba482f1f278aea895c7bc6
ChaoGaoUCR <cgao037@ucr.edu>
2025-04-01 01:26:39 +0000

    new file:   CC_baseline.log 	new file:   SSNP_baseline.log 	new file:   SSWP_baseline.log 	new file:   Viterbi_baseline.log 	modified:   march21_bfs_ssr_sssp.sh 	new file:   march21_sswp_ssnp_viterbi.sh 	new file:   march21_sswp_ssnp_viterbi.txt 	new file:   march31_CC.txt

--- march21_bfs_ssr_sssp.sh
+++ march21_bfs_ssr_sssp.sh
@@ -1,7 +1,7 @@
-bash BFS_baseline.sh &> BFS_baseline.log
-bash SSR_baseline.sh &> SSR_baseline.log
-bash SSSP_baseline.sh &> SSSP_baseline.log
+# bash BFS_baseline.sh &> BFS_baseline.log
+# bash SSR_baseline.sh &> SSR_baseline.log
+# bash SSSP_baseline.sh &> SSSP_baseline.log
 bash SSWP_baseline.sh &> SSWP_baseline.log
 bash SSNP_baseline.sh &> SSNP_baseline.log
 bash Viterbi_baseline.sh &> Viterbi_baseline.log
-bash WCC_baseline.sh &> CC_baseline.log
+# bash WCC_baseline.sh &> CC_baseline.log
